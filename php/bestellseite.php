8e64cf00f5d40fb349f150d85f854f9bcc32128a
Christoph <xtofamberg@googlemail.com>
2023-07-22 10:32:04 +0200

    Updated legal and help issues
    included how-to-information (anleitungen)
    minor formating changes

--- php/bestellseite.php
+++ php/bestellseite.php
@@ -20,19 +20,22 @@ setlocale(LC_ALL, "de_DE.UTF8");
         <link rel="stylesheet" href="../css/jquery-ui.css">
         <link rel="stylesheet" href="../css/jquery-ui.theme.css">
         <link rel="stylesheet" href="../css/jquery-ui.structure.css">
-        <link rel="stylesheet" href="../css/fontawesome/css/all.min.css" />
+        <link rel="stylesheet" href="../css/fontawesome/css/all.min.css">
         <link rel="stylesheet" href="../css/fontawesome/css/solid.css" >
+        <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.dataTables.min.css">  
         <link rel="stylesheet" href="../css/meineFormatierungenBlau.css">
+ 
     </head>
 
     <body>
     
-        <nav class="navbar navbar-expand-lg bg-primary navbar-dark">
+
+        <nav class="navbar bg-primary navbar-dark">
+        <div class="navbar-header">
             <a class="navbar-brand" href="abmelden.php" id="titel"><?php echo($_SESSION["benutzer"]); ?> abmelden</a>
-            <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
-            <li class="nav-item">
-                <a class="nav-link" href="javascript:schreibMail()"><div id="ansprechpartner"></div></a>
-            </li>
+        </div>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="nav-item"> <a class="nav-link" href="javascript:schreibMail()"><div id="ansprechpartner"></div></a></li>
             </ul>
         </nav>
         <br>
@@ -52,16 +55,24 @@ setlocale(LC_ALL, "de_DE.UTF8");
                     </tr>
                     <tr>
                         <td>
-                            <div id="buttonleiste" style="float: left; width: 100%;"></div>
-                            <div style="float: none ;"></div>
-                            <div id="buttonleiste2" style="float: right; margin-right: 0px;"></div>
-                    <tr>
-                        <td class="zentriertesTab">
-                        <div id="output">&nbsp;</div>
+                            <div style="float: left; ">
+                                     <div id="buttonleiste" ></div>
+                                     <div id="buttonleiste3 "><input type="search" id="suche" class="form-control suche" placeholder="Suche"></div>
+                            </div>
+                            <div class="conditionalAlignment">
+                                       <div id="buttonleiste2" ></div> 
+                                       <div id="buttonleiste4"></div>
+                            </div>     
                         </td>
-                    </tr>
-                    </td>
-                    </tr>
+                     </tr>
+                </table>
+            </div>
+        </div>
+        
+       
+
+        <div id="output"><table id="seitentabelle" class="table"  style="width:100%;"><thead style="background-color: white"><tr><th></th><th>Auswahl</th><th data-priority="11">Buch</th><th  data-priority="1">Jgst.</th><th data-priority="1">Fach</th><th>Verlag</th><th data-priority="2">Preis</th><th data-priority="2">Ihr Code</th></tr></thead></table></div>
+                        <table>      
                     <tr>
                         <td class="zentriertesTab">
                             <div id="platzhalter" style="font-size:small">&nbsp;</div>
@@ -69,14 +80,14 @@ setlocale(LC_ALL, "de_DE.UTF8");
                     </tr>
                     <tr></tr>
                 </table>
-            </div>
-        </div>
-
+    
         <script type="text/javascript" src="../js/jquery-3.6.1.min.js"></script>
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
         <script type="text/javascript" src="../datatables/datatables.js" defer></script>
         <script type="text/javascript" src="../js/jquery.dataTables.min.js" defer></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js" defer></script>
+    
         <script type="text/javascript" src="../js/dataTables.buttons.min.js" defer></script>
         <script type="text/javascript" src="../js/dataTables.select.min.js" defer></script>
         <script type="text/javascript" src="../js/moment.min.js" defer></script>
@@ -85,13 +96,14 @@ setlocale(LC_ALL, "de_DE.UTF8");
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js" defer></script>
         <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js" defer></script>
         <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js" defer></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/fixedheader/3.4.0/js/dataTables.fixedHeader.min.js" defer></script>
         <script src="../js/jquery-ui.js"></script>
 
         <script type="text/javascript" src="../js/controllerBestellungen.js"></script>
 
         <!-- Modale Dialoge -->
         <div style="display:none;">
-            <div id="dialog-kaufen" title="Ausgew&auml;hlte E-Book-Codes bestellen?">
+            <div id="dialog-kaufen" title="Ausgew&auml;hlte E-Book-Codes bestellen?" style="max-width:600px" >
                 <table>
                     <th></th>
                     <th></th>
@@ -100,21 +112,13 @@ setlocale(LC_ALL, "de_DE.UTF8");
                          <div style="background-color: white; border:0px"><i class="fa-solid fa-triangle-exclamation fa-2xl" style="float:left; color:orange; margin:12px 12px 20px 0;"></i></div>
                         </td>
                         <td>Wollen Sie die ausgew&auml;hlten E-Book-Codes wirklich bestellen?<br>
-                           <b> Hierf&uuml;r fallen Kosten in H&ouml;he von <div id="Kosten"  style="display: inline;">&nbsp;</div> an!</b>
+                           <b> Hierf&uuml;r fallen Kosten in H&ouml;he von <div id="Kosten"  style="display: inline;">&nbsp;</div> an!</b><br>
+                           <table style="margin-top: 3px"><tr><td style="vertical-align:top"><input type=checkbox id="widerrufsrechtEinverstanden"/></td>
+                           <td><div id="widerrufsverzichtserklaerung" style="font-size:0.8em">Hiermit erteile ich meine Zustimmung zur unverz&uumlglichen Ausf&uumlhrung des Vertrags und nehme zur Kenntnis, dass ich mein Recht auf Widerruf des Vertrags verliere, sobald der digitale Inhalt zur Verf&uumlgung gestellt wurde.</div></td>
+                            </tr></table>
                         </td>
                     </tr>
                 </table>
-            </div>
-        </div>
-
-        <div style="display:none;">
-            <div id="dialog-hochladen" title="Eintr&auml;ge hinzuf&uuml;gen">
-                <div id="dateiInfo" style="text-align: center">&nbsp;</div>
-                <form id="formular" "enctype=" multipart/form-data">
-                    <label for="inputFile" style="width: 100%">
-                        <div class="btn btn-outline-success" id="ohne" value="Datei ausw&auml;hlenn" style="width: 100%">Datei ausw&auml;hlen</div>
-                    </label>
-                    <div style="display:none;"><input type="file" multiple="multiple" id="inputFile" /></div>
             </div>
         </div>
 
